643ebd45d94678bdf9452ad2e2a30e3cb6b3767a
Emilio Del Castillo <emilio.del-castillo@outlook.com>
2021-04-22 01:27:32 +0200

    Add activity indicator

--- Components/Search.tsx
+++ Components/Search.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { StyleSheet, Button, TextInput, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Button, TextInput, Text, View, FlatList, ActivityIndicator } from 'react-native';
 import { getFilmsFromAPIWithSearchedText } from '../API/TMDBapi';
 import type { APIResult } from '../API/TMDBapi'
 
@@ -20,12 +20,23 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+
+    activity_indicator: {
+        position: 'absolute', // Afficher par dessus tout
+        alignItems: 'center', // Centrer
+        justifyContent: 'center',
+        top: 100, // Laisser de l'espace pour les inputs en haut de l'écran
+        left: 0, // Mais se coller aux autres bords pour que le spinner soit bien au milieu
+        right: 0,
+        bottom: 0
     }
 })
 
 // Pour pouvoir donner une propriété à state, on crée un nouveau type
 type State = {
-    films: Film[]
+    films: Film[],
+    isLoading: boolean
 }
 
 // Il faut une valeur par défaut pour props avant de pouvoir donner le type de state, d'où le {}
@@ -34,20 +45,37 @@ class Search extends React.Component<{}, State> {
     searchString: string
     constructor(props: {}) {
         super(props)
-        this.state = { films: defaultFilms }
+        this.state = {
+            films: defaultFilms,
+            isLoading: false
+        }
         this.searchString = ""
     }
 
     private loadFilms() {
         if (this.searchString.length > 0) {
+            this.setState({ isLoading: true })
             getFilmsFromAPIWithSearchedText<APIResult>(this.searchString).then(data => {
-                this.setState({ films: data.results })
+                this.setState({
+                    films: data.results,
+                    isLoading: false
+                })
             })
         }
     }
 
     private updateSearchString(text: string) {
         this.searchString = text
+    }
+
+    private displayActivityIndicator() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.activity_indicator}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
     }
 
     render() {
@@ -73,6 +101,7 @@ class Search extends React.Component<{}, State> {
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({ item }) => <FilmItem film={item} />}
                 />
+                {this.displayActivityIndicator()}
             </View>
         )
     }
